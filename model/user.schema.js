519179d597b40a270449e0f1c32b0517b3f2738d
kartik0603 <kartikhirapara800@gmail.com>
2024-12-17 17:34:49 +0530

    ..

--- model/user.schema.js
+++ model/user.schema.js
@@ -30,28 +30,7 @@ const userSchema = new mongoose.Schema(
 );
 
 
-// Hash the password before saving 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-      return next(); 
-    }
-  
-    try {
-      this.password = await bcrypt.hash(this.password, 10);
-      next(); 
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  //compare the entered password with the stored hashed password
-  userSchema.methods.comparePassword = async function (candidatePassword) {
-    try {
-      return await bcrypt.compare(candidatePassword, this.password);
-    } catch (error) {
-      throw new Error("Error comparing password");
-    }
-  };
+
   
   const User = mongoose.model("User", userSchema); 
 
